7ddc387fadb70e571b9b4da83be0f9546eed5323
RASCUNHO papel e design <breno.castro.ofc@gmail.com>
2025-05-02 23:27:30 -0300

    Implements getters and setters in entities

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,7 +1,8 @@
+import { Entity } from './../../core/entities/entity';
+import dayjs from 'dayjs';
 import { Optional } from './../../core/@types/optional';
 import { UniqueEntityID } from './../../core/entities/unique-entity-id';
 import { Slug } from "./value-objects/slug";
-import { Entity } from "@/core/entities/entity";
 
 interface IQuestionRequest {
   authorId: UniqueEntityID;
@@ -14,12 +15,65 @@ interface IQuestionRequest {
 }
 export class Question extends Entity<IQuestionRequest> {
 
+  get authorId() {
+    return this.props.authorId;
+  }
+  
+  get bestAnswerId() {
+    return this.props.bestAnswerId;
+  }
+  
+  get title() {
+    return this.props.title;
+  }
+
+  get content() {
+    return this.props.content;
+  }
+  
+  get slug() {
+    return this.props.slug;
+  }
+  
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get isNew(): boolean {
+    return dayjs().diff(this.createdAt) <= 3;
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  set title(title: string) {
+    this.props.content = title;
+    this.props.slug = Slug.process(title);
+    this.touch();
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId;
+    this.touch();
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+
   static create(
-    props: Optional<IQuestionRequest, 'createdAt'>,
+    props: Optional<IQuestionRequest, 'createdAt' | 'slug'>,
     id?: UniqueEntityID
   ) {
     const question = new Question({
       ...props,
+      slug: props.slug ?? Slug.process(props.title),
       createdAt: new Date()
     }, id);
     return question;
